9d4900848b578cf69669449842091d738ec07f0b
Mateus <teuspersi@gmail.com>
2021-07-15 21:22:35 -0300

    first commit

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,27 @@
+import { response } from "express";
+
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+class CreateUserUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ email, name }: IRequest): any {
+    const userAlreadyExists = this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("Email is already taken");
+    }
+
+    const user = this.usersRepository.create({ name, email });
+
+    return user;
+  }
+}
+
+export { CreateUserUseCase };
---
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -0,0 +1,26 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class TurnUserAdminUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User {
+    const user = this.usersRepository.findById(user_id);
+
+    console.log(user_id);
+
+    if (!user) {
+      throw new Error("User does not exist");
+    }
+
+    const updatedUser = this.usersRepository.turnAdmin(user);
+
+    return updatedUser;
+  }
+}
+
+export { TurnUserAdminUseCase };
